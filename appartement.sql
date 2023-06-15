0658ae1e50513191bb938434a6d3409a24d97472
BV <97569576+BVCoca@users.noreply.github.com>
2023-06-15 14:40:15 +0200

    Update appartement.sql

--- appartement.sql
+++ appartement.sql
@@ -1,8 +1,6 @@
 -- phpMyAdmin SQL Dump
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1:3306
 -- Généré le : jeu. 15 juin 2023 à 08:38
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.4.9
@@ -12,12 +10,7 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
---
 -- Base de données : `php_evaluation_benjamin`
 --
 
@@ -41,6 +34,3 @@ CREATE TABLE IF NOT EXISTS `appartement` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
